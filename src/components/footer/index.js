8968ffec5a27e37bf4e8c32a5c7980e6eaf5bb42
cheddar <nowaczewskijack@gmail.com>
2023-01-08 15:15:14 -0500

    add new class name for footer, change NavBar text color

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,16 +4,16 @@ import "./footer.css";
 function Footer() {
   return (
     <section>
-      <p>
+      <p className="footer-item">
         <a href=""> Contact Me!</a>
       </p>
-      <p>
+      <p className="footer-item">
         <a href="https://github.com/Lil-Chevy">GitHub</a>
       </p>
-      <p>
+      <p className="footer-item">
         <a href="https://www.linkedin.com/in/jack-nowaczewski/">LinkedIn</a>
       </p>
-      <p>
+      <p className="footer-item">
         <a href="https://www.codewars.com/users/Jack_spicy">CodeWars</a>
       </p>
     </section>
