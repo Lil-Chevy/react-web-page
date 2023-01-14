d3d7d27fdf8ffc68f402fa9bdf747dc413a2b050
cheddar <nowaczewskijack@gmail.com>
2023-01-14 09:06:47 -0500

    edit NavBar path for projects, add content boxes to home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,7 +4,18 @@ import "./home.css";
 function Home() {
   return (
     <section>
-      <div>Displaying Home Page</div>
+      <div id="content-box">
+        <p className="home-content">Hello! And Welcome to My Web Page!</p>
+        <p className="home-content">
+          Feel free to look around and enjoy the website!
+        </p>
+      </div>
+      <div id="content-box">
+        <p className="home-content">
+          Please feel free to follow the links below to see places like my
+        </p>
+        <p className="home-content">Github Repo, Code Wars profile, and more</p>
+      </div>
     </section>
   );
 }
