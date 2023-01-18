40863eb584a00cb8b58a827d8505be9d3630af01
cheddar <nowaczewskijack@gmail.com>
2023-01-18 14:42:06 -0500

    add downloadable Resume pdf to Resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./resume.css";
+import DownloadDoc from "../../assets/resumeDownload/JackNowaczewskiResume.pdf";
 
 function Resume() {
   return (
     <section>
-      <div>Displaying Resume Page</div>
+      <div>Please feel free to review and download my resume! </div>
+      <button className="resume-button">
+        <a
+          className="resume-button"
+          href={DownloadDoc}
+          download="JackNowaczewskiResume.pdf"
+        >
+          Download My Resume
+        </a>
+      </button>
     </section>
   );
 }
