7ee87838740eb4f3d77d546e106cfaa403e339be
cheddar <nowaczewskijack@gmail.com>
2023-01-21 19:21:44 -0500

    add image for refernce

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,17 +1,25 @@
 import React from "react";
 import "./about.css";
 import "../../App.css";
+import AboutPhoto from "../../assets/Photos/weddingPhoto.jpg";
 
 function About() {
   return (
-    <section className="yeet">
+    <section className="section">
       <div className="mx-auto p-4 py-md-5">
         <div>
-          <h1>Hey there!</h1>
+          <h1>Hey there, This is me (and my Wife)!</h1>
+          <div className="photo-box">
+            <img
+              className="about-photo"
+              src={AboutPhoto}
+              alt="Photograph of the Author"
+            />
+          </div>
           <p className="fs-5 col-md-8">
             Welcome to my page! I have gone on quite the journey to get to this
             point. I have always wanted how to learn how to work on computers,
-            and I never thought I'd learn how to code on them!{" "}
+            and I never thought I'd learn how to code on them!
           </p>
           <div className="row g-5">
             <div className="col-md-6">
