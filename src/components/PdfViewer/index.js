f51de4d0972edbb766a3d0457d46a1a1800b07cc
cheddar <nowaczewskijack@gmail.com>
2023-01-26 20:44:54 -0500

    change PDFViewer to img tag

--- src/components/PdfViewer/index.js
+++ src/components/PdfViewer/index.js
@@ -1,25 +1,19 @@
-import React, { useState } from "react";
-import { Document, Page } from "react-pdf";
-import ViewDocument from "../../assets/resumeDownload/JackNowaczewskiResume.pdf";
+import React from "react";
+import "./pdfViewer.css";
+import ResumePhotoOne from "../../assets/resumePhotos/pageOneResume.png";
+import ResumePhotoTwo from "../../assets/resumePhotos/pageTwoResume.png";
 
 function PdfViewer() {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-    setPageNumber(1);
-  }
-
   return (
-    <div>
-      <Document file={ViewDocument} onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
-    </div>
+    <section>
+      <div>
+        <img src={ResumePhotoOne} alt="resume page one" className="resumeOne" />
+      </div>
+      <div>
+        <img src={ResumePhotoTwo} alt="resume page two" className="resumeTwo" />
+      </div>
+      <p></p>
+    </section>
   );
 }
 
