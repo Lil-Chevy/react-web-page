10a8e530a203fb36e3a180b5aff9bcb2b4b8ed77
cheddar <nowaczewskijack@gmail.com>
2023-02-04 15:51:36 -0500

    remove footer until resolution is found

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import About from "./components/About";
 import Resume from "./components/Resume";
 import Projects from "./components/Projects";
 import TestingPage from "./components/TestingPage";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -26,7 +26,7 @@ function App() {
         <Route path="/Projects" element={<Projects />} />
         <Route path="/TestingPage" element={<TestingPage />} />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -6,21 +6,21 @@ function Footer() {
   return (
     <section className="footer">
       {/* Contact Links */}
-      {/* <p className="footer-item">
+      <p className="footer-item">
         <a href="/contact"> Contact Me!</a>
-      </p> */}
+      </p>
       {/* GitHub Link */}
-      {/* <p className="footer-item">
+      <p className="footer-item">
         <a href="https://github.com/Lil-Chevy">GitHub</a>
-      </p> */}
+      </p>
       {/* LinkedIn Link */}
-      {/* <p className="footer-item">
+      <p className="footer-item">
         <a href="https://www.linkedin.com/in/jack-nowaczewski/">LinkedIn</a>
-      </p> */}
+      </p>
       {/* CodeWars Profile Link */}
-      {/* <p className="footer-item">
+      <p className="footer-item">
         <a href="https://www.codewars.com/users/Jack_spicy">CodeWars</a>
-      </p> */}
+      </p>
     </section>
   );
 }
