f1057dd89259aa8fdaff4017c907053c3e1e1216
cheddar <nowaczewskijack@gmail.com>
2023-02-09 15:05:11 -0500

    add react carousel component and to the project page

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,13 +1,39 @@
 import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Smokies from "../../assets/Photos/weddingPhoto.jpg";
 import "./carousel.css";
 
 // carousel is for projects to rotate on Projects page
-function Carousel() {
+function CarouselComponent() {
   return (
-    <section>
-      <div>{/* add carousel component from bootstrap */}</div>
+    <section className="carousel-container">
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 firstPhoto"
+            src={Smokies}
+            alt="slide one of project one"
+          />
+          <Carousel.Caption>
+            <h3>First Slide Label</h3>
+            <p>Paragraph</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100 firstPhoto"
+            src={Smokies}
+            alt="slide one of project one"
+          />
+          <Carousel.Caption>
+            <h3>First Slide Label</h3>
+            <p>Paragraph</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </section>
   );
 }
 
-export default Carousel;
+export default CarouselComponent;
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import CarouselComponent from "../Carousel";
 import "./projects.css";
 
 function Projects() {
   return (
     <section>
       <div>Displaying Projects Page</div>
+      <CarouselComponent />
     </section>
   );
 }
