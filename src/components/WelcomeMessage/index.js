db347aa26655b61e3de88eec241a051a402b517e
cheddar <nowaczewskijack@gmail.com>
2023-02-27 18:35:00 +0100

    add dynamic use of currentLocation for url and messsage section

--- src/components/WelcomeMessage/index.js
+++ src/components/WelcomeMessage/index.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CurrentPage from "../CurrentPage";
 import "../../App.css";
+
 // this component dynamically renders the welcome message in the header of the home page
 function WelcomeMessage() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    if (CurrentPage === "/") {
+      setMessage("Welcome");
+    } else if (CurrentPage === "/About") {
+      setMessage("About Me");
+    } else if (CurrentPage === "/Resume") {
+      setMessage("My Resume");
+    } else if (CurrentPage === "/Projects") {
+      setMessage("Projects");
+    } else {
+      setMessage("Contact Me!");
+    }
+  }, []);
+
   return (
     <a
       href="/"
       className="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none"
     >
       <svg className="bi me-2" width="40" height="32"></svg>
-      <span className="fs-4"> Welcome</span>
+      <span className="fs-4">{message}</span>
     </a>
   );
 }
