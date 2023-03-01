fe438e180eb9bb5df75b4cc70d24d627ea839d27
cheddar <nowaczewskijack@gmail.com>
2023-03-01 16:07:41 -0500

    add styling to about page and according classes

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,7 @@ import AboutPhoto from "../../assets/Photos/weddingPhoto.jpg";
 
 function About() {
   return (
-    <section className="section">
+    <section className="about-page-section">
       <div className="mx-auto p-4 py-md-5">
         <div>
           <h1>Hey there, This is me (and my Wife)!</h1>
@@ -16,7 +16,7 @@ function About() {
               alt="Photograph of the Author"
             />
           </div>
-          <p className="fs-5 col-md-8">
+          <p className="fs-5 col-md-8 about-page-paragraph">
             Welcome to my page! I have gone on quite the journey to get to this
             point. I have always wanted how to learn how to work on computers,
             and I never thought I'd learn how to code on them!
