d2c79fab49597e4d602030fe87e9df888aed1486
cheddar <nowaczewskijack@gmail.com>
2023-03-03 15:45:23 -0500

    add correct notation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,14 +4,14 @@ import "./home.css";
 function Home() {
   return (
     <section className="home">
-      {/* greeting */}
+      {/* Greeting Div*/}
       <div id="content-box">
         <p className="home-content">Hello! And Welcome to My Web Page!</p>
         <p className="home-content">
           Feel free to look around and enjoy the website!
         </p>
       </div>
-      {/* information section */}
+      {/* Information Section */}
       <div id="content-box">
         <p className="home-content">
           Please feel free to follow the links below to see places like my
